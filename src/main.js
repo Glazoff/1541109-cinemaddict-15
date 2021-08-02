96a76ac9ba95c510cd9635a1e1b619cb276107bd
Женя <84893786+Glazoff@users.noreply.github.com>
2021-08-02 19:03:48 +0300

    ДЗ 2.16. Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import {createFilterStatsTemplate} from './view/filter-stats.js';
 import {createFilмList} from './view/film-list.js';
 import {createCardFilm} from './view/card-film.js';
-import {createFilmListTopRated} from './view/film-list-top-rated';
-import {createFilmListMostComment} from './view/film-list-most-comment.js';
 import {createUserRating} from './view/user-rating.js';
 import {createShowMore} from './view/show-more.js';
 //import {createPopupFilmDetails} from './view/popup-film-details.js';
 import {generateFilm} from './mock/film.js';
 
 const COUNT_FILMS_LIST = 5;
-const COUNT_FILMS_EXTRA = 2;
 //const body = document.querySelector('body');
 
 const films = new Array(COUNT_FILMS_LIST).fill().map(generateFilm);
@@ -32,27 +29,6 @@ const filmsList = document.querySelector('.films-list');
 const filmsListContainer = filmsList.querySelector('.films-list__container');
 for (let i = 0; i < COUNT_FILMS_LIST; i++){
   render(filmsListContainer, createCardFilm(films[i]), 'afterbegin');
-}
-
-const movies = document.querySelector('.films');
-
-
-//Отрисовываем фильмы с наибольшим колличеством рейтинга
-render(movies ,createFilmListTopRated(), 'beforeend');
-const filmListTopRated = movies.querySelector('.films-list--extra:nth-child(2)');
-const filmsTopRated = filmListTopRated.querySelector('.films-list__container');
-for (let i = 0; i < COUNT_FILMS_EXTRA; i++){
-  render(filmsTopRated, createCardFilm(films[i]), 'afterbegin');
-}
-
-
-
-//Отрисовываем фильмы с наибольшим колличеством комментариев
-render(movies ,createFilmListMostComment(), 'beforeend');
-const filmListMostComment = movies.querySelector('.films-list--extra:nth-child(3)');
-const filmsMostComment = filmListMostComment.querySelector('.films-list__container');
-for (let i = 0; i < COUNT_FILMS_EXTRA; i++){
-  render(filmsMostComment, createCardFilm(films[i]), 'afterbegin');
 }
 
 
--- src/view/film-list-top-rated.js
+++
@@ -1,8 +0,0 @@
-export const createFilmListTopRated = () => (
-  `<section class="films-list films-list--extra">
-    <h2 class="films-list__title">Top rated</h2>
-      <div class="films-list__container">
-      </div>
-    </section>
-  `
-);
